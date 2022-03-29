b316bca25f6129746a54d45439b8b9e3cd0867a0
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-03-29 07:27:24 -0300

    feat: main index

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const openNav = () => {
+    document.querySelector(".siderbar").style.width = "250px"
+    document.querySelector("main").style.marginLeft = "250px"
+}
+
+const closeNav = () => {
+    document.querySelector(".siderbar").style.width = "0"
+    document.querySelector("main").style.marginLeft = "0"
+}
+
